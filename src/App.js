45aead6304024683017f33d5b061232f123b35da
ifrazie <ifrazie@wgu.edu>
2025-04-02 16:08:04 -0400

    add image carousel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import MeditationTimer from "./components/MeditationTimer";
 import "./styles/App.css"; // Add a new CSS file for global styles
 
@@ -15,10 +17,25 @@ function App() {
         {darkMode ? "Light Mode" : "Dark Mode"}
       </button>
       <h1>Meditation Timer</h1>
+      {/* Add the image carousel */}
+      <Carousel autoPlay infiniteLoop showThumbs={false}>
+        <div>
+          <img src="/images/soothing1.jpg" alt="Soothing Scene 1" />
+        </div>
+        <div>
+          <img src="/images/soothing2.jpg" alt="Soothing Scene 2" />
+        </div>
+        <div>
+          <img src="/images/soothing3.jpg" alt="Soothing Scene 3" />
+        </div>
+        <div>
+          <img src="/images/soothing4.jpg" alt="Soothing Scene 4" />
+        </div>
+      </Carousel>
       <MeditationTimer />
       <p>Focus on your breathing and relax.</p>
       <footer>
-        <p>© 2023 Meditation Timer. All rights reserved.</p>
+        <p>© 2023 Nekura Systems. All rights reserved.</p>
       </footer>
     </div>
   );
